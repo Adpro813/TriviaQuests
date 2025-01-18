3035af736aebae8d9cd61ca863b3995259d4e454
Adpro813 <adityad1108@gmail.com>
2025-01-17 21:23:37 -0800

    added basic log in page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,15 +4,34 @@ function LoginScreen() {
   return (
     <div className="login-screen-container">
       <div className="header">
-        <img src="logo.png" class="logo"></img>
+        <img src="/logo.png" alt="TriviaQuests Logo" className="logo" />
       </div>
 
       <div className="login-form-container">
         <div className="login-form">
-          <p className="login-text">hey</p>
+          <div className="form-fields">
+            <input 
+              type="email" 
+              placeholder="Email" 
+              className="login-input"
+            />
+            <div className="password-container">
+              <input 
+                type="password" 
+                placeholder="Password" 
+                className="login-input"
+              />
+              <button className="show-password-btn">
+                👁️
+              </button>
+            </div>
+            <button className="sign-in-btn">Sign In</button>
+            <a href="#" className="forgot-password">Forgot password?</a>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
 export default LoginScreen;
